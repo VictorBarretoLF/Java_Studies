8fa433a57be9eba53fe48b6e562ea7f8af90c875
Víctor Barreto <victorbarretolins@gmail.com>
2023-07-30 14:05:49 -0300

    initializing project

---
+++ spring-cloud-open-feign-intro/src/main/java/com/springfeignintro/SpringCloudOpenFeignIntroApplication.java
@@ -0,0 +1,13 @@
+package com.springfeignintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOpenFeignIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudOpenFeignIntroApplication.class, args);
+	}
+
+}
