ce52424e0fc7290fe3af73e3ca8c819f5ad3133d
Víctor Barreto <victor.barreto@behoh.com>
2023-08-10 17:31:53 -0300

    feat: equality in hibernate

--- spring-6-webapp/src/main/java/grupo/springframework/spring6webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/grupo/springframework/spring6webapp/domain/Book.java
@@ -1,5 +1,6 @@
 package grupo.springframework.spring6webapp.domain;
 
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -19,7 +20,7 @@ public class Book {
 	private String title;
 	private String isbn;
 
-	@ManyToMany(mappedBy = "authors")
+	@ManyToMany
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
 	private Set<Author> authors;
 
@@ -53,6 +54,28 @@ public class Book {
 
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", authors=" + authors + "]";
 	}
 
 }
