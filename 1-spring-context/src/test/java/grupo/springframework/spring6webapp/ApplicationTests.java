b00686097e40ceeee331a90c88b540c869be51ac
Víctor Barreto <victor.barreto@behoh.com>
2023-09-06 17:22:35 -0300

    chore: Spring test context

--- 1-spring-context/src/test/java/grupo/springframework/spring6webapp/ApplicationTests.java
+++ 1-spring-context/src/test/java/grupo/springframework/spring6webapp/ApplicationTests.java
@@ -1,10 +1,32 @@
 package grupo.springframework.spring6webapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import grupo.springframework.spring6webapp.controllers.MyController;
 
 @SpringBootTest
 class ApplicationTests {
+	
+    @Autowired
+    ApplicationContext applicationContext;
+	
+    @Autowired
+    MyController myController;
+    
+    @Test
+    void testAutowireOfController() {
+        System.out.println(myController.sayHello());
+    }
+    
+    @Test
+    void testGetControllerFromCtx() {
+        MyController myController = applicationContext.getBean(MyController.class);
+
+        System.out.println(myController.sayHello());
+    }
 
 	@Test
 	void contextLoads() {
