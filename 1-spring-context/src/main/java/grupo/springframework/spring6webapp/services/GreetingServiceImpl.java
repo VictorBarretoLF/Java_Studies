55206336676a99603cbe3a76a2c75bec6936b02b
Víctor Barreto <victor.barreto@behoh.com>
2023-09-15 18:05:57 -0300

    chore: injeção de dependencia usando o spring para o GreetingServiceImpl

--- 1-spring-context/src/main/java/grupo/springframework/spring6webapp/services/GreetingServiceImpl.java
+++ 1-spring-context/src/main/java/grupo/springframework/spring6webapp/services/GreetingServiceImpl.java
@@ -1,7 +1,11 @@
 package grupo.springframework.spring6webapp.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingServiceImpl implements GreetingService {
 
+	@Override
 	public String sayGreeting() {
 		return "Hello Everyone from base service!!!";
 	}
