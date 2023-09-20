2b45e94fc6503d6a96f437e9f8ebc84dbeaa3f55
˜Victor <victorbarretolins@gmail.com>
2023-09-19 22:14:30 -0300

    feat: http post spring MVC

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/controller/BeerController.java
@@ -1,8 +1,15 @@
 package guru.springframework.spring6restmvc.controller;
 
+import java.util.List;
 import java.util.UUID;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.service.BeerService;
@@ -11,15 +18,30 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
+    
+    @PostMapping
+    //@RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity handlePost(@RequestBody Beer beer){
 
-    public Beer getBeerById(UUID id){
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
         log.debug("Get Beer by Id - in controller");
 
-        return beerService.getBeerById(id);
+        return beerService.getBeerById(beerId);
     }
 
 }
--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerService.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/service/BeerService.java
@@ -1,11 +1,17 @@
 package guru.springframework.spring6restmvc.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import guru.springframework.spring6restmvc.model.Beer;
 
 public interface BeerService {
 
+    List<Beer> listBeers();
+
     Beer getBeerById(UUID id);
+
+    Beer saveNewBeer(Beer beer);
 }
+
 
