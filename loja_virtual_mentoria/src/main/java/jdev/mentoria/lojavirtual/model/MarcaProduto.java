17db53890864c7e36dbc06a106f9dc672f847548
˜Victor <victorbarretolins@gmail.com>
2023-10-21 12:52:06 -0300

    fix: fixed the generatedValue for the id

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
@@ -1,5 +1,7 @@
 package jdev.mentoria.lojavirtual.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,13 +19,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "marca_produto")
 @SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto {
+public class MarcaProduto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long Id;
 	
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private String nomeDesc;
 
 }
