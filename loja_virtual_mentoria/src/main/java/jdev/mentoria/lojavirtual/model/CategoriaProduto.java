07ba23bc9d6cb367b340b6417b5b956298e6e94a
Víctor Barreto <victor.barreto@behoh.com>
2023-10-23 17:34:41 -0300

    chore: criado categoria produto

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -20,16 +20,15 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @EqualsAndHashCode
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
-	private Long Id;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	private Long id;
 	
-	@Column(nullable = false)
+	@Column(name = "nome_desc", nullable = false)
 	private String nomeDesc;
-
 }
