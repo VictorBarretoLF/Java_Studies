5ab01c4fc2b160e6561878e66a19cfea79f44b60
Víctor Barreto <victor.barreto@behoh.com>
2023-10-30 16:43:02 -0300

    chore: Adicionando enums de status para ContaPagar e ContaReceber e atualizando TipoEndereco com ajustes de formatação.

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/enums/StatusContaReceber.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/enums/StatusContaReceber.java
@@ -1,24 +1,22 @@
 package jdev.mentoria.lojavirtual.enums;
 
-public enum TipoEndereco {
-
-	COBRANCA("Cobrança"), 
-	ENTREGA("Entrega");
-
+public enum StatusContaReceber {
+	
+	COBRANCA("Pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada");
+	
 	private String descricao;
-
-	private TipoEndereco(String descricao) {
+	
+	private StatusContaReceber(String descricao) {
 		this.descricao = descricao;
 	}
-
+	
 	public String getDescricao() {
 		return descricao;
 	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
+	
 	@Override
 	public String toString() {
 		return this.descricao;
