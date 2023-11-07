7235e3b9b81c7e65d26151c869da8c3e56a9c48d
Víctor Barreto <victor.barreto@behoh.com>
2023-11-07 10:27:52 -0300

    feat: criado compose para spring data jpa

---
+++ sdjpa-intro/src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -0,0 +1,5 @@
+package guru.springframework.sdjpaintro;
+
+public class MySQLIntegrationTest {
+
+}
--- sdjpa-intro/src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ sdjpa-intro/src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -1,10 +1,25 @@
 package guru.springframework.sdjpaintro;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import guru.springframework.sdjpaintro.repositories.BookRepository;
 
 @SpringBootTest
 class SdjpaIntroApplicationTests {
+	
+    @Autowired
+    BookRepository bookRepository;
+    
+    @Test
+    void testBookRepository() {
+        long count = bookRepository.count();
+
+        assertThat(count).isGreaterThan(0);
+    }
 
 	@Test
 	void contextLoads() {
