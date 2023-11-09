5f97a7df1d58defa014c9027c200b9eefbef88c4
Víctor Barreto <victor.barreto@behoh.com>
2023-11-09 09:45:21 -0300

    feat: adicionado nova coluna a tabela de livros

--- sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ sdjpa-intro/src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -23,16 +23,16 @@ public class DataInitializer implements CommandLineRunner {
 
         // Array of generated books
         Book[] generatedBooks = new Book[]{
-            new Book("Optional interactive capability", "9781759084046", "Mack Group"),
-            new Book("Automated exuding strategy", "9780499517166", "Glass, Smith and Matthews"),
-            new Book("Switchable local benchmark", "9781609557867", "Owen Ltd"),
-            new Book("Operative fault-tolerant policy", "9780326854945", "Edwards-Reed"),
-            new Book("Enhanced disintermediate utilization", "9780053881993", "Mitchell, Pratt and Grant"),
-            new Book("Function-based interactive Internet solution", "9781034741336", "Johnson, Mccarty and Weber"),
-            new Book("Progressive non-volatile solution", "9780041858617", "Davis, Kent and Mann"),
-            new Book("Upgradable non-volatile analyzer", "9780854438907", "Gallagher-Love"),
-            new Book("Function-based disintermediate moderator", "9781880326442", "Bailey and Sons"),
-            new Book("Organized multi-state moderator", "9780554391526", "Wood-Riley")
+            new Book("Optional interactive capability", "9781759084046", "Mack Group", null),
+            new Book("Automated exuding strategy", "9780499517166", "Glass, Smith and Matthews", null),
+            new Book("Switchable local benchmark", "9781609557867", "Owen Ltd", null),
+            new Book("Operative fault-tolerant policy", "9780326854945", "Edwards-Reed", null),
+            new Book("Enhanced disintermediate utilization", "9780053881993", "Mitchell, Pratt and Grant", null),
+            new Book("Function-based interactive Internet solution", "9781034741336", "Johnson, Mccarty and Weber", null),
+            new Book("Progressive non-volatile solution", "9780041858617", "Davis, Kent and Mann", null),
+            new Book("Upgradable non-volatile analyzer", "9780854438907", "Gallagher-Love", null),
+            new Book("Function-based disintermediate moderator", "9781880326442", "Bailey and Sons", null),
+            new Book("Organized multi-state moderator", "9780554391526", "Wood-Riley", null)
         };
 
         // Save each generated book to the repository
