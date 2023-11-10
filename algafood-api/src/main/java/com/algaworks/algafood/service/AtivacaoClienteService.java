77efa0c76d8f950d182799f3e73b58346a7f7b9f
Víctor Barreto <victor.barreto@behoh.com>
2023-11-10 18:08:37 -0300

    aprendendo IOC

---
+++ algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.modelo.Cliente;
+import com.algaworks.algafood.notificacao.Notificador;
+
+@Component
+public class AtivacaoClienteService {
+
+	private Notificador notificador;
+	
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//		
+//		System.out.println("AtivacaoClienteService: " + notificador);
+//	}
+//	
+//	public AtivacaoClienteService(String qualquer) {
+//		
+//	}
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+	}
+	
+}
