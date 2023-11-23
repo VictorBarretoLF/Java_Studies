d2703f4ce79b2dccb240f2598d4d776d5ed096a3
˜Victor <victorbarretolins@gmail.com>
2023-11-22 22:32:16 -0300

    modelando xml

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
@@ -19,15 +21,18 @@ public class CozinhaController {
 	private CozinhaRepository cozinhaRepository;
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Cozinha> listar1() {
-		System.out.println("LISTAR 1");
+	public List<Cozinha> listarJSON() {
 		return cozinhaRepository.listar();
 	}
 	
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public List<Cozinha> listar2() {
-		System.out.println("LISTAR 2");
-		return cozinhaRepository.listar();
+	public CozinhasXmlWrapper listarXML() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+	
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscarCoinhaPorId(@PathVariable Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
 	}
 
 }
